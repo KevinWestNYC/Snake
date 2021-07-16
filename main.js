e8fc95c4b83d08051d3c8db79cb33d13fe9190c7
Kevin W <kevinwestnyc@gmail.com>
2021-07-15 20:30:19 -0400

    html started

---
+++ main.js
@@ -0,0 +1,7 @@
+// event listeners for the keys 
+// make snake move in desired direction
+// make last snake cube go to the location of the box before it
+// dont let snake go opposite direction 
+// game over if hits wall or itself
+// get lead snake box location
+// grow snake after each apple 
